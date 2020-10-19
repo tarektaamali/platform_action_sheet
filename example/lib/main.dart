d7b8b6d05575dc3a4bdd9d9be1871da54193e1b7
tarektaamali <tarek.taamali@gmail.com>
2020-10-19 22:26:43 +0100

    add documentation

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,41 +1,5 @@
-# Platform action sheet
-
-[![Platform](https://img.shields.io/badge/Platform-Flutter-brightgreen)](https://flutter.dev/)
-[![Pub](https://img.shields.io/badge/Pub-V0.0.3-blue)](https://pub.dev/packages/platform_action_sheet)
-
-
-This package is a simple platform specific actionSheet for Cupertino and Android with the following options:
-- Text - *(Required)* String to display
-- onPressed - *(Required)* Function to be executed on the option item being pressed
-- hasArrow - Not visible on IOS, indicating more options will appear
-- isCancel
-- defaultAction
-
-
-## Getting started
-In the pubspec.yaml of your flutter project, add the following dependency:
-
-```dart
-dependencies:
-  ...
-  platform_action_sheet: 
-    git:
-      url: https://github.com/tarektaamali/platform_action_sheet.git
-```
-In your library add the following import:
-```dart
-import 'package:platform_action_sheet/platform_action_sheet.dart';
-```
-For help getting started with Flutter, view the [online documentation](https://flutter.io/).
-
-## Example
-![](https://github.com/Amazing-Aidan/platform_action_sheet/blob/master/art/android.png?raw=true)
-
-
-```dart
 import 'package:flutter/material.dart';
 import 'package:platform_action_sheet/platform_action_sheet.dart';
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -64,26 +28,26 @@ class Home extends StatelessWidget {
               color: Colors.blue,
               onPressed: () => PlatformActionSheet().displaySheet(
                   context: context,
-                  title: Row(
-                    children: <Widget>[
                   actions: [
                     ActionSheetAction(
                       text: "Edit",
                       colortrailingIcon: Colors.green,
                       trailingIcon: Icons.edit,
                       onPressed: () => Navigator.pop(context),
+                      //hasArrow: true,
                     ),
                     ActionSheetAction(
-                      text: "delete",
+                        text: "delete",
                       trailingIcon: Icons.delete,
                       colortrailingIcon: Colors.red,
                       onPressed: () => Navigator.pop(context),
                     ),
                     ActionSheetAction(
                       text: "Cancel",
-                      trailingIcon: Icons.close
+                      trailingIcon: Icons.close,
+                       colortrailingIcon: Colors.red,
                       onPressed: () => Navigator.pop(context),
-                      isCancel: true,
+                    //  isCancel: true,
                       defaultAction: true,
                     )
                   ]),
@@ -97,4 +61,3 @@ class Home extends StatelessWidget {
     );
   }
 }
-```
